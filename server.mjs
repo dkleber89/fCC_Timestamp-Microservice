d88010a92854a3a60c0030d71e46e16e27f1cb17
dkleber89 <dkleber89@gmail.com>
2020-01-11 10:27:36 +0100

    Basic Setup

---
+++ server.mjs
@@ -0,0 +1,33 @@
+// server.mjs
+// where your node app starts
+
+// init project
+import express from 'express';
+import cors from 'cors';
+import { gDirname } from './utils/utilityFunctions.mjs';
+
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', (req, res) => {
+  res.sendFile(`${gDirname(import.meta.url)}/views/index.html`);
+});
+
+// your first API endpoint...
+app.get('/api/hello', (req, res) => {
+  res.json({ greeting: 'hello API' });
+});
+
+// listen for requests :)
+const port = process.env.PORT || 3000;
+
+const listener = app.listen(port, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
